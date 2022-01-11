886758b236c088e85f3e603836f44a498f2424f7
DoiXuanDat <doixuandat1@gmail.com>
2022-01-11 22:28:16 +0700

    .

--- process-signup.php
+++ process-signup.php
@@ -2,7 +2,7 @@
     //Một số kỹ thuận validate form được xử lý  ở Js
     //tôi coi các vấn đề không tồn tại ở đây
     if(!isset($_POST['btnSignup'])) {
-        header("location:signup.php"); 
+        header("location:SignUp.php"); 
     }
 
     //toi coi moi du lieu la hop le
@@ -18,17 +18,17 @@
         die("Kết nối thất bại. Vui Lòng kiểm tra lại các thông tin máy chủ");
         }
     //Bước 02: Thực hiện truy vấn
-    $sql01 = "Select * from db_account where email = '$email' ";
+    $sql01 = "Select * from account where email = '$email' ";
     //ở đây còn có các vấn đề về tính hợp lệ dl nhập vào form
 
     $result = mysqli_query($conn,$sql01);
 
     if(mysqli_num_rows($result) > 0){
         $error = "Email is existed";
-        header("location: signup.php?error=$error"); //Chuyển hướng về trang quản trị
+        header("location: SignUp.php?error=$error"); //Chuyển hướng về trang quản trị
     }
     else{
-        $sql02 = "Insert into db_account(email, matkhau) values ('$email', '$pass') ";
+        $sql02 = "Insert into account(email, matkhau) values ('$email', '$pass') ";
         $result02 = mysqli_query($conn,$sql02);
         if($result02 == TRUE){
             header("location: login.php");
